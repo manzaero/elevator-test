6dac77e9b86023e3bb7f26e161243a0fbb1d71d4
manzaero <ligoero@gmail.com>
2023-09-30 21:05:31 +0300

    addnig in project sass

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div class="building">
+    <div class="elevator">
 
+    </div>
+    <div class="floor">
+      <button>#</button>
+      <p></p>
+    </div>
   </div>
 </template>
 
@@ -10,6 +16,129 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.building {
+  position: relative;
+  margin: 0 auto;
+  button, p {
+    margin-left: 120px;
+  }
+  button {
+    width: 20px;
+    height: 20px;
+    border: 1px solid #1ff;
+  }
+
+}
+
+/* Reset and base styles  */
+* {
+  padding: 0px;
+  margin: 0px;
+  border: none;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+/* Links */
+
+a,
+a:link,
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+/* Common */
+
+aside,
+nav,
+footer,
+header,
+section,
+main {
+  display: block;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  font-size: inherit;
+  font-weight: inherit;
+}
+
+ul,
+ul li {
+  list-style: none;
+}
+
+img {
+  vertical-align: top;
+}
+
+img,
+svg {
+  max-width: 100%;
+  height: auto;
+}
+
+address {
+  font-style: normal;
+}
+
+/* Form */
+input,
+textarea,
+button,
+select {
+  font-family: inherit;
+  font-size: inherit;
+  color: inherit;
+  background-color: transparent;
+}
+
+input::-ms-clear {
+  display: none;
+}
+
+button,
+input[type="submit"] {
+  display: inline-block;
+  box-shadow: none;
+  background-color: transparent;
+  background: none;
+  cursor: pointer;
+}
+
+input:focus,
+input:active,
+button:focus,
+button:active {
+  outline: none;
+}
+
+button::-moz-focus-inner {
+  padding: 0;
+  border: 0;
+}
+
+label {
+  cursor: pointer;
+}
+
+legend {
+  display: block;
+}
 </style>
